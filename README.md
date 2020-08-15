73b6941d63381ce5efbadc1059b3b5eb97b8fa84
Francisco Miamoto <fsmiamoto@gmail.com>
2020-08-15 20:20:12 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# C
+
+Just some example C code to serve as reference.
+
+Most of it is taken from the examples given by Prof. Jacob Sober in his amazing [YouTube  channel](https://www.youtube.com/c/JacobSorber).
---
+++ little.c
@@ -0,0 +1,9 @@
+/*
+ * The actual entrypoint is not `main` but `_start`
+ * So this is one the of the smallest programs you can write
+ * To compile: clang -nostdlib little.c
+ */
+void _start() {
+  while (1) {
+  }
+}
